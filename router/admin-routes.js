bcddfed67f91670f3169a76e3f2c0afdd96c5c46
raja <102196806+SMRPcoder@users.noreply.github.com>
2023-07-16 09:46:02 +0530

    admin update

--- router/admin-routes.js
+++ router/admin-routes.js
@@ -9,7 +9,7 @@ AdminRouter.get("/getRolesList",auth_admin,AdminController.viewRolesList);
 AdminRouter.post("/adduser",auth_admin,upload.single("file"),AdminController.addUser);
 
 // view all
-AdminRouter.post("/viewAllEmployee",auth_admin,AdminController.viewAllEmployee);
+AdminRouter.post("/viewAllEmployees",auth_admin,AdminController.viewAllEmployees);
 AdminRouter.post("/viewAllUsers",auth_admin,AdminController.viewAllUsers);
 
 // view single
@@ -21,10 +21,13 @@ AdminRouter.post("/editEmployee",auth_admin,AdminController.editEmployee);
 AdminRouter.post("/editUser",auth_admin,AdminController.editUser);
 
 // delete
-AdminRouter.post("/deleteUser",auth_admin,AdminController.deleteUser)
+AdminRouter.post("/deleteUser",auth_admin,AdminController.deleteUser);
 
 
-
+// role permission
+AdminRouter.post("/createRolePermission",auth_admin,AdminController.createRolePermission);
+AdminRouter.post("/addPermission",auth_admin,AdminController.addPermission);
+AdminRouter.post("/removePermission",auth_admin,AdminController.removePermission);
 
 
 module.exports=AdminRouter;
